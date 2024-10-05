ccfd5568e53c8c0ab91052e9d5c22f30cb7d4584
youcefs21 <youcefs2121@gmail.com>
2024-10-05 19:31:57 -0400

    python quest

--- abstractPOC/src/index.ts
+++ abstractPOC/src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+import { parse, Lang, kind } from "@ast-grep/napi";
+import fs from "node:fs";
+import { type FlowKind, flowKinds } from "./kinds";
+
+const filePath = "PythonQuest/main.py";
+const source = fs.readFileSync(filePath, "utf-8");
+const ast = parse(Lang.Python, source);
+const root = ast.root();
+
+const children = root.children();
+// console.log(new Set(children.map((x) => x.kind())));
+const flow = children.filter((x) => flowKinds.includes(x.kind() as FlowKind));
+
+console.log(flow.map((x) => x.kind()));
