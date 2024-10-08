92ba8b45c6c2ad4e6a372a75bb5dd0655ce26840
youcefs21 <youcefs2121@gmail.com>
2024-10-08 19:37:28 -0400

    idk just backing up

--- POC/app/components/CustomData.ts
+++ POC/app/components/CustomData.ts
@@ -2,9 +2,10 @@ import type { Node } from "@xyflow/react";
 
 export type CustomData = {
 	text: string;
-	groupId: string;
 	og_pos: { x: number; y: number };
-	onExpand: (parent: Node<CustomData>) => void;
+	onExpand: (node: CustomNode) => void;
+	groupId: string | null;
+	expanded: boolean;
 };
 
 export type CustomNode = Node<CustomData>;
