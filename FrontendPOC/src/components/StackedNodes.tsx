00f7bbd25cf4daecf53fa0123dd95896b9527158
ahmetdumlu <erdemdumlu1@gmail.com>
2024-11-22 11:35:46 -0500

    lol

--- FrontendPOC/src/components/StackedNodes.tsx
+++ FrontendPOC/src/components/StackedNodes.tsx
@@ -4,33 +4,31 @@ import { Variable } from "lucide-react";
 
 export function StackedNodes({ data }: { data: CustomData }) {
 	return (
-		<div className="flex flex-col">
-			<div className="bg-transparent rounded-lg p-2 overflow-hidden flex flex-col">
-				{(() => {
-					if (data.id === "-2") return null;
+		<div className="bg-transparent rounded-lg p-2 overflow-hidden">
+			{(() => {
+				if (data.id === "-2") return null;
 
-					const nodeData = {
-						label: data.label,
-						id: `${data.children[0].id}-root`,
-						hasChildren: false,
-						idRange: data.idRange,
-						children: [],
-						reversed: false,
-						active: false,
-						disabled: false,
-						type: data.type,
-					} satisfies CustomData;
+				const nodeData = {
+					label: data.label,
+					id: `${data.children[0].id}-root`,
+					hasChildren: false,
+					idRange: data.idRange,
+					children: [],
+					reversed: false,
+					active: false,
+					disabled: false,
+					type: data.type,
+				} satisfies CustomData;
 
-					const Component = nodeTypes[nodeData.type];
-					return <Component key={nodeData.id} data={nodeData} />;
-				})()}
-				<div className="pl-4 flex flex-col">
-					{data.children.map((n) => {
-						const Component = nodeTypes[n.type];
+				const Component = nodeTypes[nodeData.type];
+				return <Component key={nodeData.id} data={nodeData} />;
+			})()}
+			<div className="pl-4 flex flex-col">
+				{data.children.map((n) => {
+					const Component = nodeTypes[n.type];
 
-						return <Component key={n.id} data={n} />;
-					})}
-				</div>
+					return <Component key={n.id} data={n} />;
+				})}
 			</div>
 		</div>
 	);
